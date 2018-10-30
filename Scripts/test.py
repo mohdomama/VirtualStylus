58d24064fea4079cc0eddd684423d02cf0c2cb6a
Mohd Omama <mohd.omama@gmail.com>
2018-10-30 08:18:45 +0530

    feat: add volume control

---
+++ Scripts/test.py
@@ -0,0 +1,3 @@
+import os
+import util
+print(os.getcwd())
---
+++ util/SystemUtil.py
@@ -0,0 +1,28 @@
+import alsaaudio
+
+
+class AudioUtility:
+
+    def __init__(self):
+        self.m = alsaaudio.Mixer()
+
+    def increaseVolume(self):
+        vol = self.m.getvolume()
+        newVol = min(100, int(vol[0]) + 10)
+        print(newVol)
+        self.m.setvolume(newVol)
+
+    def decreaseVolume(self):
+        vol = self.m.getvolume()
+        newVol = max(0, int(vol[0] - 10))
+        self.m.setvolume(newVol)
+
+
+def main():
+    util = AudioUtility()
+    util.increaseVolume()
+    util.increaseVolume()
+
+
+if __name__ == '__main__':
+    main()

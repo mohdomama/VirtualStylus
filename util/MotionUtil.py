6a277e799d84d580fb572c53d7a589e71c375e1d
Mohd Omama <mohd.omama@gmail.com>
2018-11-17 06:56:22 +0530

    feat: add motion for new classes

--- util/MotionUtil.py
+++ util/MotionUtil.py
@@ -4,84 +4,44 @@ from util.SystemUtil import AudioUtility
 audio = AudioUtility()
 
 class Actions(object):
-    clicked = False
-
     
     def palm_action(movement, diff):
         command = "xte 'keydown Control_L' 'keydown Alt_L' 'keydown {}' 'keyup Control_L' 'keyup Alt_L' 'keyup {}'".format(movement, movement)
         os.system(command)
     
 
-    def fist_action(movement, diff):
+    def curve_action(movement, diff):
         if movement == 'Right':
             audio.increaseVolume()
         elif movement == 'Left':
             audio.decreaseVolume()
+            #command = "xte 'keydown Page_Up'  'keyup Page_Up'"
+            #os.system(command)
 
         elif movement == 'Up':
             command = "xte 'keydown Control_L' 'keydown Alt_L' 'keydown T' 'keyup Control_L' 'keyup Alt_L' 'keyup T'"
             os.system(command)
-            pass
 
         else:
             command = "xte 'keydown Super_L' 'keydown L' 'keyup Super_L' 'keyup L'"
             os.system(command)
 
-    
 
-    
-    '''
-    def palm_action(movement, diff):
-        if diff > 20:
-            diff = diff*4
-        else:
-            diff = 20
-        command = "xte 'mouseup i'"
-        os.system(command)
-
-
+    def angle_action(movement, diff):
         if movement == 'Right':
-            command = "xte 'mousermove {} 0'".format(diff)
-            os.system(command)
-
+            pass
         elif movement == 'Left':
-            command = "xte 'mousermove -{} 0'".format(diff)
-            os.system(command)
+            pass
+            #command = 
+            #os.system(command)
 
         elif movement == 'Up':
-            command = "xte 'mousermove 0 -{}'".format(diff)
+            command = "xte 'keydown Page_Up'  'keyup Page_Up'"
             os.system(command)
 
-        elif movement == 'Down':
-            command = "xte 'mousermove 0 {}'".format(diff)
-            os.system(command)
-
-
-    def fist_action(movement, diff):
-        if diff > 20:
-            diff = diff*4
         else:
-            diff = 20
-        command = "xte 'mousedown i'"
-        os.system(command)
-
-        if movement == 'Right':
-            command = "xte 'mousermove {} 0'".format(diff)
+            command = "xte 'keydown Page_Down'  'keyup Page_Down'"
             os.system(command)
-
-        elif movement == 'Left':
-            command = "xte 'mousermove -{} 0'".format(diff)
-            os.system(command)
-
-        elif movement == 'Up':
-            command = "xte 'mousermove 0 -{}'".format(diff)
-            os.system(command)
-
-        elif movement == 'Down':
-            command = "xte 'mousermove 0 {}'".format(diff)
-            os.system(command)
-    '''
-    
         
 
 class Motion(object):
@@ -89,23 +49,18 @@ class Motion(object):
     def __init__(self):
         self.base_pos = {}
         self.base_pos['Palm'] = None
-        self.base_pos['Fist'] = None
+        self.base_pos['Curve'] = None
+        self.base_pos['Angle'] = None
 
         self.actions = {}
         self.actions['Palm'] =  Actions.palm_action
-        self.actions['Fist'] = Actions.fist_action
+        self.actions['Curve'] = Actions.curve_action
+        self.actions['Angle'] = Actions.angle_action
         
         self.motion_thresh = {}
         self.motion_thresh['Palm'] = (70, 70) #Horizontal, Vertical
-        self.motion_thresh['Fist'] = (50, 100) #Horizontal, Vertical
-
-
-    def palm_action(self, movement):
-        pass
-
-
-    def fist_action(self, movement):
-        pass
+        self.motion_thresh['Curve'] = (50, 100) #Horizontal, Vertical
+        self.motion_thresh['Angle'] = (30, 30) #Horizontal, Vertical
 
 
     def detect(self, new_pos, gesture_name):
@@ -143,9 +98,10 @@ class Motion(object):
         for key, value in self.base_pos.items():
             self.base_pos[key] = None
 
-    def clear_gesture(gesture_name):
+    def clear_gesture(self, gesture_name):
         for key, value in self.base_pos.items():
             if key != gesture_name:
                 self.base_pos[key] = None
+
 
      
